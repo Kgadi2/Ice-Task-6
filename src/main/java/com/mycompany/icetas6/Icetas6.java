cfea2267d9b2f51cbcb81a5d26d3a384515173ab
RC_Student_lab <RC_Student_lab@10.0.0.94>
2024-10-04 19:30:51 +0000

    First Commit

---
+++ src/main/java/com/mycompany/icetas6/Icetas6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.icetas6;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Icetas6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
